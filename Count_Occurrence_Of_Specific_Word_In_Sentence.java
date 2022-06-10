7e97a2ebbe3af5e01a8b207856af8525f8617512
Suraj Birajdar <dadabirajdar323@gmail.com>
2022-06-10 23:19:08 +0530

    Initial commit

---
+++ Count_Occurrence_Of_Specific_Word_In_Sentence.java
@@ -0,0 +1,22 @@
+package programs;
+
+public class Count_Occurrence_Of_Specific_Word_In_Sentence {
+	public static void main(String[] args) {
+		//program to count occurrences of specific word in a sentence
+		String sentence = "My name is Suraj is is";
+		String [] words = sentence.split(" ");
+		String specificWord = "is";
+		int count=0;
+		for ( int i=0; i<words.length; i++) {
+		    if(words[i].equals(specificWord)){
+		        count++;
+		    }
+		}
+		System.out.println("Count of number of occurrences of word in a sentence is: " + count);
+	}
+}
+
+
+
+
+
