e643d33cb4ba766627c4d9ac3b9e20f4da459818
suraj <dadabirajdar323@gmail.com>
2022-11-28 00:07:25 +0530

    Count Number of digits

---
+++ eclipse-workspace/JavaPrograms/src/CountNumberOfDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CountNumberOfDigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		long num=sc.nextLong();
+		int count=0;
+		while(num>0) {
+			num=num/10;
+			count++;
+		}
+        System.out.println(count);
+	}
+
+}
