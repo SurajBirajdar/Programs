65a4add727274a1e6891fa6ad9f1e6bd540dd2b8
SurajBirajdar <78243934+SurajBirajdar@users.noreply.github.com>
2024-10-19 16:18:47 +0530

    Java Programs added

---
+++ program/ReplaceCharWithItsOccurence.java
@@ -0,0 +1,38 @@
+package programs.program;
+
+import java.util.Arrays;
+
+public class ReplaceCharWithItsOccurence {
+
+    public static void main(String[] args) {
+        String str = "opentext";
+        char charToReplace = 't';
+
+        if(str.indexOf(charToReplace) == -1) {
+            System.out.println("Given character not present in string");
+            System.exit(0);
+        }
+        int cnt = 1;
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == charToReplace) {
+                str = str.replaceFirst(String.valueOf(charToReplace),String.valueOf(cnt));
+                cnt++;
+            }
+        }
+        System.out.println(str);
+
+        /*
+        int cnt = 1;
+        //now character present in string but I want to replace t with its occurrence
+        char[] charArray = str.toCharArray();
+        for(int i=0; i<charArray.length; i++) {
+            if(charArray[i] == charToReplace) {
+                charArray[i] = String.valueOf(cnt).charAt(0);
+                cnt++;
+            }
+        }
+        System.out.println(Arrays.toString(charArray)); */
+
+
+    }
+}
