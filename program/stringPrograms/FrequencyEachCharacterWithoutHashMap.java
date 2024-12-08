28807be0aa116ef6aa911bb20b7afb0fe08d0dc6
SurajBirajdar <78243934+SurajBirajdar@users.noreply.github.com>
2024-12-08 17:54:58 +0530

    Program added of frequency of each character in string without using hashmap

--- program/stringPrograms/FrequencyEachCharacterWithoutHashMap.java
+++ program/stringPrograms/FrequencyEachCharacterWithoutHashMap.java
@@ -5,31 +5,49 @@ public class FrequencyEachCharacterWithoutHashMap {
 
     public static void main(String[] args) {
 
-        String s1 = "surajs";
-        char[] arr = s1.toCharArray();
-        int fr[] = new int[arr.length];
+        String s1 = "surajsu";
+        frequencyOfEachCharWithouthashMap(s1);
+//        char[] arr = s1.toCharArray();
+//        int fr[] = new int[arr.length];
+//
+//        int visited = -1;
+//        int count = 1;
+//
+//        for (int i = 0; i < arr.length; ++i) {
+//            count = 1;
+//            for (int j = i + 1; j < arr.length; ++j) {
+//                if (arr[i] == arr[j]) {
+//                    ++count;
+//                    fr[j] = visited;
+//                }
+//            }
+//            if (fr[i] != visited) {
+//                fr[i] = count;
+//            }
+//        }
+//
+//        for (int i = 0; i < fr.length; ++i) {
+//            if (fr[i] != visited) {
+//                System.out.println(arr[i] + "-->" + fr[i]);
+//            }
+//        }
+    }
 
-        int visited = -1;
-        int count = 1;
+    public static void frequencyOfEachCharWithouthashMap(String str) {
 
-        for (int i = 0; i < arr.length; ++i) {
-            count = 1;
-            for (int j = i + 1; j < arr.length; ++j) {
-                if (arr[i] == arr[j]) {
-                    ++count;
-                    fr[j] = visited;
+        for(int i=0; i<str.length(); i++) {
+            int count = 0;
+            for(int j=0; j<str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+                if(j<i && str.charAt(i) == str.charAt(j)) {
+                    break;
                 }
             }
-            if (fr[i] != visited) {
-                fr[i] = count;
+            if(count != 0) {
+                System.out.println("Frequency of character " + str.charAt(i) + " is: " + count);
             }
         }
-
-        for (int i = 0; i < fr.length; ++i) {
-            if (fr[i] != visited) {
-                System.out.println(arr[i] + "-->" + fr[i]);
-            }
-        }
-
     }
 }
