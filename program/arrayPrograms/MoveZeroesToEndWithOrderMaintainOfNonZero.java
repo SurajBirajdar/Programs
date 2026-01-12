d4d4e5289cb6e4ff43e1b1bab2e159ac77ec49f6
SurajBirajdar <78243934+SurajBirajdar@users.noreply.github.com>
2026-01-12 15:49:47 +0530

    Add implementations for moving zeroes to the end of an array

--- program/arrayPrograms/MoveZeroesToEndWithOrderMaintainOfNonZero.java
+++ program/arrayPrograms/MoveZeroesToEndWithOrderMaintainOfNonZero.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class MoveZeroesToEndWithOrderMaintainOfNonZero {
     public static void main(String[] args) {
         // Write a Java program to move all 0s to the end of the array, while keeping the order of non-zero elements the same.
-        int[] arr = {0,1,0,3,12};
+        /*int[] arr = {0,1,0,3,12};
         int length = arr.length;
         List<Integer> newArr = new ArrayList<>();
         int count=1;
@@ -19,5 +19,24 @@ public class MoveZeroesToEndWithOrderMaintainOfNonZero {
             count++;
         }
         System.out.println(newArr.toString());
+        */
+
+        //=====================Another approach=========================//
+        // Write a Java program to move all 0s to the end of the array, while keeping the order of non-zero elements the same.
+        int[] arr = {0,1,0,3,12};
+        int length = arr.length;
+        int count=0;
+        for(int i=0; i<length; i++) {
+            if(arr[i] != 0) {
+                arr[count] = arr[i];
+                count++;
+            }
+        }
+        while(count < length) {
+            arr[count] = 0;
+            count++;
+        }
+        System.out.println(Arrays.toString(arr));
+
     }
 }
